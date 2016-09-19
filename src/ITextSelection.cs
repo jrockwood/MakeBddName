9fcb98d5a57738b31842f2dae828eb6535988422
Justin Rockwood <justinrockwood@hotmail.com>
2016-09-19 07:21:59 -0700

    Adding ITextSelection for unit testing.
    It needs to be filled in still.

---
+++ src/ITextSelection.cs
@@ -0,0 +1,18 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="ITextSelection.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All rights reserved. Licensed under the Apache License, Version 2.0.
+//   See LICENSE in the project root for license information.
+// </copyright>
+// <created>2016-09-19</created>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace MakeBddName
+{
+    /// <summary>
+    /// Contains an abstraction for the Visual Studio <see cref="TextSelection"/> functionality.
+    /// Made into an interface to support unit testing.
+    /// </summary>
+    internal interface ITextSelection
+    {
+    }
+}
