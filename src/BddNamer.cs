587e53d1ca483aeec4e82e06fb6c66687d2ebd28
Justin Rockwood <justinrockwood@hotmail.com>
2017-03-26 23:28:31 -0700

    Add BddNamer.ToPascalCase()

--- src/BddNamer.cs
+++ src/BddNamer.cs
@@ -9,6 +9,8 @@
 namespace MakeBddName
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Text;
 
     public static class BddNamer
@@ -17,23 +19,110 @@ namespace MakeBddName
         /// Converts a string from sentence-style to a valid C# identifier with underscores between words.
         /// </summary>
         /// <param name="sentence">The sentence-style string to convert.</param>
-        /// <returns></returns>
-        public static string ToUnderscoreName(string sentence)
+        /// <param name="makeSentence">
+        /// If true, converts all but the first word to lower case; otherwise the casing of the words
+        /// is not changed.
+        /// </param>
+        /// <returns>A valid C# identifier with underscores between words.</returns>
+        /// <example>"This is my method name" becomes "This_is_my_method_name"</example>
+        public static string ToUnderscoreName(string sentence, bool makeSentence = false)
+        {
+            IEnumerable<string> words = NormalizeSentence(sentence, nonAlphaNumbericReplacementChar: '_');
+            if (makeSentence)
+            {
+                // ReSharper disable PossibleMultipleEnumeration
+                words = words.Take(1).Concat(words.Skip(1).Select(word => word.ToLowerInvariant()));
+                // ReSharper restore PossibleMultipleEnumeration
+            }
+
+            return string.Join("_", words);
+        }
+
+        /// <summary>
+        /// Converts a string from sentence-style to a valid C# identifier using PascalCase, where
+        /// each word begins with a capital letter and is concentenated together.
+        /// </summary>
+        /// <param name="sentence">The sentence-style string to convert.</param>
+        /// <returns>A valid C# identifier using PascalCase.</returns>
+        /// <example>"This is my method name" becomes "ThisIsMyMethodName"</example>
+        public static string ToPascalCase(string sentence)
+        {
+            IEnumerable<string> words = NormalizeSentence(sentence, nonAlphaNumbericReplacementChar: null);
+            words = words.Select(word => char.ToUpperInvariant(word[0]) + word.Substring(1)).ToArray();
+            return string.Join("", words);
+        }
+
+        /// <summary>
+        /// Normalizes a sentence into words.
+        /// </summary>
+        /// <param name="sentence">
+        /// The raw sentence, which could be a combination of sentence-style (spaces between words),
+        /// PascalCase, or underscore '_' delimited words.
+        /// </param>
+        /// <param name="nonAlphaNumbericReplacementChar">
+        /// The character to use for substituting non-alphanumeric characters.
+        /// </param>
+        /// <returns>An array of words.</returns>
+        private static IEnumerable<string> NormalizeSentence(string sentence, char? nonAlphaNumbericReplacementChar)
         {
             if (sentence == null) { throw new ArgumentNullException(nameof(sentence)); }
 
             // remove the whitespace and quotes from the beginning and the end
             string trimmed = sentence.Trim(' ', '\t', '\n', '\r', '\f', '"', '\'');
-            var builder = new StringBuilder(trimmed.Length);
-            foreach (char c in trimmed)
+
+            // replace all non-alphanumeric characters
+            var builder = new StringBuilder(trimmed);
+            for (int i = 0; i < builder.Length; i++)
             {
-                builder.Append(char.IsLetterOrDigit(c) ? c : '_');
+                char c = builder[i];
+                if (!char.IsLetterOrDigit(c) && !char.IsWhiteSpace(c))
+                {
+                    builder[i] = nonAlphaNumbericReplacementChar.GetValueOrDefault(' ');
+                }
             }
 
-            // trim off extra underscores from the beginning and the end of the string
-            string convertedString = builder.ToString();
-            convertedString = convertedString.Trim('_');
-            return convertedString;
+            trimmed = builder.ToString();
+
+            // trim any non-alphanumeric character replacements
+            if (nonAlphaNumbericReplacementChar.HasValue)
+            {
+                trimmed = trimmed.Trim(nonAlphaNumbericReplacementChar.Value);
+            }
+
+            // split the string into words
+            var words = new List<string>();
+            builder.Clear();
+            foreach (char c in trimmed)
+            {
+                // we hit a word boundary, so add the word and start a new one
+                if (char.IsWhiteSpace(c) || c == nonAlphaNumbericReplacementChar.GetValueOrDefault('\0'))
+                {
+                    words.Add(builder.ToString());
+                    builder.Clear();
+                }
+                // an upper-case letter is also a word boundary
+                else if (char.IsLetter(c) && char.IsUpper(c))
+                {
+                    if (builder.Length > 0)
+                    {
+                        words.Add(builder.ToString());
+                        builder.Clear();
+                    }
+                    builder.Append(c);
+                }
+                else
+                {
+                    builder.Append(c);
+                }
+            }
+
+            // add the last word
+            if (builder.Length > 0)
+            {
+                words.Add(builder.ToString());
+            }
+
+            return words.Where(word => !string.IsNullOrWhiteSpace(word));
         }
     }
 }
--- src/MakeBddNameTests/BddNamerTests.cs
+++ src/MakeBddNameTests/BddNamerTests.cs
@@ -41,7 +41,7 @@ namespace MakeBddNameTests
             [Test]
             public void should_replace_non_alpha_numeric_characters()
             {
-                BddNamer.ToUnderscoreName("quote\"@here").Should().Be("quote__here");
+                BddNamer.ToUnderscoreName("quote\"@here").Should().Be("quote_here");
             }
 
             [Test]
@@ -60,6 +60,100 @@ namespace MakeBddNameTests
             public void should_return_an_empty_string_if_all_whitespace_or_non_alpha_numeric()
             {
                 BddNamer.ToUnderscoreName("  @%^*  ").Should().BeEmpty();
+            }
+
+            [Test]
+            public void should_convert_from_a_pascal_string()
+            {
+                BddNamer.ToUnderscoreName("APascalString").Should().Be("A_Pascal_String");
+            }
+
+            [Test]
+            public void should_convert_from_a_pascal_string_to_sentence_style()
+            {
+                BddNamer.ToUnderscoreName("ThisIsASentence", makeSentence: true).Should().Be("This_is_a_sentence");
+            }
+
+            [Test]
+            public void should_leave_a_single_word_untouched()
+            {
+                BddNamer.ToUnderscoreName("Word").Should().Be("Word");
+            }
+        }
+
+        public class ToPascalCase
+        {
+            [Test]
+            public void should_throw_on_invalid_args()
+            {
+                Action action = () => BddNamer.ToPascalCase(null);
+                action.ShouldThrow<ArgumentNullException>().And.ParamName.Should().Be("sentence");
+            }
+
+            [Test]
+            public void should_rename_a_quoted_string()
+            {
+                BddNamer.ToPascalCase("\"Make bdd name\"").Should().Be("MakeBddName");
+            }
+
+            [Test]
+            public void should_rename_an_unquoted_string()
+            {
+                BddNamer.ToPascalCase("make a name").Should().Be("MakeAName");
+            }
+
+            [Test]
+            public void should_remove_non_alpha_numeric_characters()
+            {
+                BddNamer.ToPascalCase("Quote\"@Here").Should().Be("QuoteHere");
+            }
+
+            [Test]
+            public void should_capitalize_all_words()
+            {
+                BddNamer.ToPascalCase("this is lower").Should().Be("ThisIsLower");
+            }
+
+            [Test]
+            public void should_start_a_new_word_after_a_number()
+            {
+                BddNamer.ToPascalCase("go 2 market").Should().Be("Go2Market");
+            }
+
+            [Test]
+            public void should_remove_non_alpha_numeric_characters_from_the_ends()
+            {
+                BddNamer.ToPascalCase("@#$hello there$&*").Should().Be("HelloThere");
+            }
+
+            [Test]
+            public void should_remove_underscores()
+            {
+                BddNamer.ToPascalCase("hello_there").Should().Be("HelloThere");
+            }
+
+            [Test]
+            public void should_return_an_empty_string_if_all_whitespace_or_non_alpha_numeric()
+            {
+                BddNamer.ToPascalCase("  @%^*  ").Should().BeEmpty();
+            }
+
+            [Test]
+            public void should_convert_from_an_underscore_string()
+            {
+                BddNamer.ToPascalCase("this_is_an_underscore_string").Should().Be("ThisIsAnUnderscoreString");
+            }
+
+            [Test]
+            public void should_leave_a_single_capitalized_word_untouched()
+            {
+                BddNamer.ToPascalCase("Word").Should().Be("Word");
+            }
+
+            [Test]
+            public void should_convert_a_single_lowercase_word_to_uppercase()
+            {
+                BddNamer.ToPascalCase("word").Should().Be("Word");
             }
         }
     }
