f58e3fbb74691bc6bfdce0da8bc01d672924a1f3
Justin Rockwood <justinrockwood@hotmail.com>
2017-03-31 17:56:38 -0700

    Rename some unit tests to be more descriptive

--- src/MakeBddNameTests/MakeBddNameCommandTests.cs
+++ src/MakeBddNameTests/MakeBddNameCommandTests.cs
@@ -22,34 +22,34 @@ namespace MakeBddNameTests
             public class Given_a_selection_in_the_middle_of_the_line
             {
                 [Test]
-                public void should_extend_to_the_quotes()
+                public void should_extend_to_the_quotes_if_quotes_are_present()
                 {
-                    var selection = new MockTextSelection("public class \"should <<do|>> something\"()");
+                    var selection = new MockTextSelection("public void \"should <<do|>> something\"()");
                     MakeBddNameCommand.ExtendSelectionToFullString(selection);
-                    selection.LineSpec.Should().Be("public class <<\"should do something\"|>>()");
+                    selection.LineSpec.Should().Be("public void <<\"should do something\"|>>()");
                 }
 
                 [Test]
-                public void should_extend_to_the_start_of_the_line_if_no_beginning_quote()
+                public void should_extend_to_the_start_of_the_line_if_no_beginning_quote_but_there_is_an_end_quote()
                 {
-                    var selection = new MockTextSelection("public class should <<do|>> something\"()");
+                    var selection = new MockTextSelection("public void should <<do|>> something\"()");
                     MakeBddNameCommand.ExtendSelectionToFullString(selection);
-                    selection.LineSpec.Should().Be("<<public class should do something\"|>>()");
+                    selection.LineSpec.Should().Be("<<public void should do something\"|>>()");
                 }
 
                 [Test]
-                public void should_extend_to_the_end_of_the_line_if_no_end_quote()
+                public void should_extend_to_the_end_of_the_line_if_no_end_quote_but_there_is_a_beginning_quote()
                 {
-                    var selection = new MockTextSelection("public class \"should <<do|>> something()");
+                    var selection = new MockTextSelection("public void \"should <<do|>> something()");
                     MakeBddNameCommand.ExtendSelectionToFullString(selection);
-                    selection.LineSpec.Should().Be("public class <<\"should do something()|>>");
+                    selection.LineSpec.Should().Be("public void <<\"should do something()|>>");
                 }
             }
 
             public class Given_a_selection_at_the_beginning_of_the_line
             {
                 [Test]
-                public void should_extend_to_the_quotes()
+                public void should_extend_to_the_quotes_if_quotes_are_present()
                 {
                     var selection = new MockTextSelection("<<|\"should do>> something\"()");
                     MakeBddNameCommand.ExtendSelectionToFullString(selection);
@@ -60,7 +60,7 @@ namespace MakeBddNameTests
             public class Given_a_selection_at_the_end_of_the_line
             {
                 [Test]
-                public void should_extend_to_the_quotes()
+                public void should_extend_to_the_quotes_if_quotes_are_present()
                 {
                     var selection = new MockTextSelection("\"should <<do something\"|>>");
                     MakeBddNameCommand.ExtendSelectionToFullString(selection);
@@ -71,43 +71,43 @@ namespace MakeBddNameTests
             public class Given_no_selection
             {
                 [Test]
-                public void should_extend_to_the_quotes()
+                public void should_extend_to_the_quotes_if_present()
                 {
-                    var selection = new MockTextSelection("public class \"should do| something\"()");
+                    var selection = new MockTextSelection("public void \"should do| something\"()");
                     MakeBddNameCommand.ExtendSelectionToFullString(selection);
-                    selection.LineSpec.Should().Be("public class <<\"should do something\"|>>()");
+                    selection.LineSpec.Should().Be("public void <<\"should do something\"|>>()");
                 }
 
                 [Test]
-                public void should_extend_to_the_start_of_the_line_if_no_beginning_quote()
+                public void should_extend_to_the_start_of_the_line_if_no_beginning_quote_but_an_ending_quote()
                 {
-                    var selection = new MockTextSelection("public class should do| something\"()");
+                    var selection = new MockTextSelection("public void should do| something\"()");
                     MakeBddNameCommand.ExtendSelectionToFullString(selection);
-                    selection.LineSpec.Should().Be("<<public class should do something\"|>>()");
+                    selection.LineSpec.Should().Be("<<public void should do something\"|>>()");
                 }
 
                 [Test]
-                public void should_extend_to_the_end_of_the_line_if_no_end_quote()
+                public void should_extend_to_the_end_of_the_line_if_no_end_quote_but_beginning_quote()
                 {
-                    var selection = new MockTextSelection("public class \"should do| something()");
+                    var selection = new MockTextSelection("public void \"should do| something()");
                     MakeBddNameCommand.ExtendSelectionToFullString(selection);
-                    selection.LineSpec.Should().Be("public class <<\"should do something()|>>");
+                    selection.LineSpec.Should().Be("public void <<\"should do something()|>>");
                 }
 
                 [Test]
                 public void should_detect_the_beginning_quote_with_the_anchor_at_the_end_of_the_line()
                 {
-                    var selection = new MockTextSelection("public class \"should do something\"|");
+                    var selection = new MockTextSelection("public void \"should do something\"|");
                     MakeBddNameCommand.ExtendSelectionToFullString(selection);
-                    selection.LineSpec.Should().Be("public class <<\"should do something\"|>>");
+                    selection.LineSpec.Should().Be("public void <<\"should do something\"|>>");
                 }
 
                 [Test]
-                public void should_detect_the_beginning_quote_with_the_anchor_not_at_the_end_of_the_line()
+                public void should_detect_the_beginning_quote_with_the_anchor_after_the_closing_quote()
                 {
-                    var selection = new MockTextSelection("public class \"should do something\"|()");
+                    var selection = new MockTextSelection("public void \"should do something\"|()");
                     MakeBddNameCommand.ExtendSelectionToFullString(selection);
-                    selection.LineSpec.Should().Be("public class <<\"should do something\"|>>()");
+                    selection.LineSpec.Should().Be("public void <<\"should do something\"|>>()");
                 }
             }
         }
