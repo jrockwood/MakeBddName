8a6b9ca9687a3697250bf13af8eb5c26645b9260
Justin Rockwood <justinrockwood@hotmail.com>
2017-04-04 22:16:52 -0700

    Update to version 1.2

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -11,7 +11,7 @@ namespace MakeBddName
         public const string Name = "Make BDD Name";
         public const string Description = @"A Visual Studio extension that adds menu commands to convert a class or method name to and from a BDD-style sentence.";
         public const string Language = "en-US";
-        public const string Version = "1.1";
+        public const string Version = "1.2";
         public const string Author = "Justin Rockwood";
         public const string Tags = "bdd, unit test";
     }
